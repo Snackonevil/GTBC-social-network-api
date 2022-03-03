f7f320f063b14b52496da6aa7787d2363fc7fb9d
Kevin Lacson <lacsonky@gmail.com>
2022-03-02 22:13:54 -0500

    connect mongoose

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const dotenv = require("dotenv").config();
-const express = require("express");
 const colors = require("colors");
-
+const express = require("express");
+const db = require("./config/connection");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`.yellow.bold);
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`.yellow.bold);
+    });
 });
