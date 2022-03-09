e1eb9b8d088d41f2d3aea5117d900e0b06e949eb
Kevin Lacson <lacsonky@gmail.com>
2022-03-08 21:28:06 -0500

    mongo url

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    "mongodb://localhost:27017/socialapi",
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
